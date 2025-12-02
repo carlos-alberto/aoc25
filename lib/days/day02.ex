b47e4284d50bdf663ff16ea7db016cdb9335fa4b
Carl Henderson <carl.s.henderson@gmail.com>
2025-12-02 09:40:04 +0000

    Day 2

--- lib/days/day02.ex
+++ lib/days/day02.ex
@@ -3,13 +3,60 @@ defmodule Day02 do
   Advent of Code - Day 02
   """
 
+  defp is_same_part_one(num) do
+    str = Integer.to_string(num)
+    half = div(byte_size(str), 2)
+
+    {first, last} = String.split_at(str, half)
+    first == last
+  end
+
+  defp parse_range(line) do
+    [a, b] =
+      line
+      |> String.split("-", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    a..b
+  end
+
   def part1(input) do
-    # TODO: Implement Part 1
     input
+    |> String.split(",", trim: true)
+    |> Enum.map(&parse_range/1)
+    |> Enum.flat_map(& &1)
+    |> Enum.filter(&is_same_part_one/1)
+    |> Enum.sum()
+  end
+
+  defp is_same_part_two(num) do
+    str = Integer.to_string(num)
+    len = byte_size(str)
+
+    if len < 2 do
+      false
+    else
+      2..len
+      |> Enum.filter(&(rem(len, &1) == 0))
+      |> Enum.any?(fn parts ->
+        chunk_size = div(len, parts)
+
+        chunks =
+          str
+          |> String.codepoints()
+          |> Enum.chunk_every(chunk_size)
+
+        length(chunks) > 1 and Enum.uniq(chunks) |> length() == 1
+      end)
+    end
   end
 
   def part2(input) do
-    # TODO: Implement Part 2
     input
+    |> String.split(",", trim: true)
+    |> Enum.map(&parse_range/1)
+    |> Enum.flat_map(& &1)
+    |> Enum.filter(&is_same_part_two/1)
+    |> Enum.sum()
   end
 end
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -1,14 +1,13 @@
 defmodule Day02Test do
   use ExUnit.Case, async: true
 
-  @example_input ""
-  # TODO: Fill @example_input with sample problem input
+  @example_input "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
   test "part1 example" do
-    assert Day02.part1(@example_input) == :todo
+    assert Day02.part1(@example_input) == 1227775554
   end
 
   test "part2 example" do
-    assert Day02.part2(@example_input) == :todo
+    assert Day02.part2(@example_input) == 4174379265
   end
 end
