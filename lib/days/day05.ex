23bca461b5620ede0628630d2bd87f1717b3e303
Carl Henderson <carl.s.henderson@gmail.com>
2025-12-05 08:36:27 +0000

    Day 5 with naive part 2 (OOM)

--- lib/days/day05.ex
+++ lib/days/day05.ex
@@ -3,13 +3,45 @@ defmodule Day05 do
   Advent of Code - Day 05
   """
 
-  def part1(input) do
-    # TODO: Implement Part 1
+  defp item_in_ranges(item, ranges) do
+    Enum.any?(ranges, fn {start, end_} -> item >= start and item <= end_ end)
+  end
+
+  defp parse_input(input) do
     input
+      |> String.split("\n")
+      |> Enum.reduce(%{ranges: [], items: [], switched: false}, fn line, acc ->
+        case line do
+          "" ->
+            %{acc | switched: true}
+
+          _ ->
+            if acc.switched do
+              %{acc | items: acc.items ++ [String.to_integer(line)]}
+            else
+              [start_s, end_s] = String.split(line, "-")
+              start = String.to_integer(start_s)
+              end_ = String.to_integer(end_s)
+              %{acc | ranges: [{start, end_} | acc.ranges]}
+            end
+        end
+      end)
+  end
+
+  def part1(input) do
+    %{ranges: ranges, items: items} = parse_input(input)
+
+    items
+    |> Enum.filter(fn item -> item_in_ranges(item, ranges) end)
+    |> length()
   end
 
   def part2(input) do
-    # TODO: Implement Part 2
-    input
+    %{ranges: ranges} = parse_input(input)
+
+    Enum.reduce(ranges, MapSet.new(), fn {start, end_}, acc ->
+      Enum.reduce(start..end_, acc, fn item, acc2 -> MapSet.put(acc2, item) end)
+    end)
+    |> MapSet.size
   end
 end
--- test/day05_test.exs
+++ test/day05_test.exs
@@ -1,14 +1,23 @@
 defmodule Day05Test do
   use ExUnit.Case, async: true
 
-  @example_input ""
-  # TODO: Fill @example_input with sample problem input
+  @example_input "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32"
 
   test "part1 example" do
-    assert Day05.part1(@example_input) == :todo
+    assert Day05.part1(@example_input) == 3
   end
 
   test "part2 example" do
-    assert Day05.part2(@example_input) == :todo
+    assert Day05.part2(@example_input) == 14
   end
 end
