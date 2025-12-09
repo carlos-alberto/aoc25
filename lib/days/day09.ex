72eef5ad539cd57e0b42955a630371309467ffb7
Carl Henderson <carl.s.henderson@gmail.com>
2025-12-09 20:16:11 +0000

    Day 9 part 1

--- lib/days/day09.ex
+++ lib/days/day09.ex
@@ -2,14 +2,55 @@ defmodule Day09 do
   @moduledoc """
   Advent of Code - Day 09
   """
+  defp calculate_rectangle_area({x1, y1}, {x2, y2}, memo) do
+    if Map.has_key?(memo, MapSet.new([{x1, y1}, {x2, y2}])) do
+      memo
+    else
+
+    width = abs(x2 - x1) + 1
+    height = abs(y2 - y1) + 1
+
+    area = width * height
+    Map.put(memo, MapSet.new([{x1, y1}, {x2, y2}]), area)
+    end
+
+  end
+
+  defp calculate_all_areas(coords) do
+    Enum.reduce(coords, %{}, fn {x1, y1}, memo ->
+      Enum.reduce(coords, memo, fn {x2, y2}, memo_acc ->
+        if {x1, y1} != {x2, y2} do
+          calculate_rectangle_area({x1, y1}, {x2, y2}, memo_acc)
+        else
+          memo_acc
+        end
+      end)
+    end)
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
 
   def part1(input) do
-    # TODO: Implement Part 1
     input
+    |> parse_input()
+    |> calculate_all_areas()
+    |> IO.inspect(label: "Calculated Areas")
+    |> Map.values()
+    |> Enum.max()
   end
 
   def part2(input) do
-    # TODO: Implement Part 2
-    input
+    red = parse_input(input)
+
+    
   end
 end
--- test/day09_test.exs
+++ test/day09_test.exs
@@ -1,14 +1,20 @@
 defmodule Day09Test do
   use ExUnit.Case, async: true
 
-  @example_input ""
-  # TODO: Fill @example_input with sample problem input
+  @example_input "7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3"
 
   test "part1 example" do
-    assert Day09.part1(@example_input) == :todo
+    assert Day09.part1(@example_input) == 50
   end
 
   test "part2 example" do
-    assert Day09.part2(@example_input) == :todo
+    assert Day09.part2(@example_input) == 24
   end
 end
