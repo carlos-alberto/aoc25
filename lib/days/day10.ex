2281522ba17aac6fee34f2d2d46343b612d6ef61
Carl Henderson <carl.s.henderson@gmail.com>
2025-12-10 09:45:59 +0000

    Day 10 part 1

--- lib/days/day10.ex
+++ lib/days/day10.ex
@@ -2,14 +2,68 @@ defmodule Day10 do
   @moduledoc """
   Advent of Code - Day 10
   """
-
-  def part1(input) do
-    # TODO: Implement Part 1
-    input
+  defp parse_buttons(button_strs) do
+    button_strs
+    |> Enum.map(fn s ->
+      s
+      |> String.slice(1..-2//1)
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+    end)
   end
 
-  def part2(input) do
-    # TODO: Implement Part 2
+  defp parse_final(final_str) do
+    final_str
+    |> String.slice(1..-2//1)
+    |> String.graphemes()
+    |> Enum.map(&(&1 == "#"))
+  end
+
+  defp solve_line(line) do
+    [end_str | button_strs] = String.split(line, " ")
+
+    buttons = parse_buttons(Enum.drop(button_strs, -1))
+
+    final = parse_final(end_str)
+
+    init_state = Enum.map(final, fn _ -> false end)
+
+    bfs(final, buttons, MapSet.new([init_state]), [{init_state, 0}])
+  end
+
+  defp bfs(final, _buttons, _visited, [{state, depth} | _]) when state == final do
+    depth
+  end
+
+  defp bfs(final, buttons, visited, [{state, depth} | queue_tail]) do
+    next_states =
+      buttons
+      |> Enum.map(fn button -> apply_button(state, button) end)
+      |> Enum.reject(&MapSet.member?(visited, &1))
+
+    new_visited =
+      Enum.reduce(next_states, visited, fn s, acc -> MapSet.put(acc, s) end)
+
+    new_queue =
+      queue_tail ++ Enum.map(next_states, fn s -> {s, depth + 1} end)
+
+    bfs(final, buttons, new_visited, new_queue)
+  end
+
+  defp apply_button(state, presses) do
+    state
+    |> Enum.with_index()
+    |> Enum.map(fn {val, idx} -> if idx in presses, do: !val, else: val end)
+  end
+
+  def part1(input) do
     input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&solve_line/1)
+    |> Enum.sum()
+  end
+
+  def part2(_input) do
+    :nottodo
   end
 end
--- test/day10_test.exs
+++ test/day10_test.exs
@@ -1,14 +1,15 @@
 defmodule Day10Test do
   use ExUnit.Case, async: true
 
-  @example_input ""
-  # TODO: Fill @example_input with sample problem input
+  @example_input "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
 
   test "part1 example" do
-    assert Day10.part1(@example_input) == :todo
+    assert Day10.part1(@example_input) == 7
   end
 
-  test "part2 example" do
-    assert Day10.part2(@example_input) == :todo
-  end
+  # test "part2 example" do
+  #   assert Day10.part2(@example_input) == :todo
+  # end
 end
